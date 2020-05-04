f76ff0ecf11810a7b5577d7d1b7253931618ef35
alexandre-hiel-rey <55872146+alexandre-hiel-rey@users.noreply.github.com>
2020-05-04 12:12:41 +0200

    Update index.php

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job 01</title>
-    <link rel="stylesheet" href="style1.css" media="(min-width: 1920px)" type="text/css" />
+    <link rel="stylesheet" href="style1.css" type="text/css" />
     <link rel="stylesheet" href="style2.css" media="(min-width: 1680px) and (max-width: 1920px)" type="text/css" />
     <link rel="stylesheet" href="style3.css" media="(min-width: 1280px) and (max-width: 1680px)"  type="text/css" />
     <link rel="stylesheet" href="style4.css" media="(max-width: 1280px)" type="text/css" />
