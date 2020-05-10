bd537e74c186067ecd92cd238a3dacd68d1ba802
alexandre-hiel-rey <55872146+alexandre-hiel-rey@users.noreply.github.com>
2020-05-11 01:09:07 +0200

    Add files via upload

---
+++ jour03/job01/index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
+    <title>Job 01</title>
+</head>
+<body class="alert-dark">
+<!-- Header -->
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">LPTF</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Units</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Jobs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Skill</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+<!-- End Header -->
+
+<!-- Titre -->
+<div class="container">
+    <div class="row justify-content-center">
+        <h1 class="text-center display-4 mb-4">LaPlateforme_</h1>
+    </div>
+</div>
+<!-- End Titre -->
+
+<main class="row d-flex justify-content-around w-100">
+    <!-- Card -->
+    <div class="card h-25" style="width: 18rem;">
+    <img src="papillon.jpg" class="card-img-top" alt="Image papillon">
+    <div class="card-body">
+        <h5 class="card-title">Un Papillon</h5>
+        <p class="card-text">Un papillon, c'est un peu comme une chenille, mais avec des ailes.<br>Ne pas ingérer.</p>
+        <a href="#" class="btn btn-primary">Commander votre propre papillon</a>
+    </div>
+    </div>
+    <!-- End Card -->
+
+    <div class="jumbotron">
+        <h1 class="display-4">Bonjour, monde!</h1>
+        <p class="lead">Il existe plusieurs visions du terme :</p>
+        <p class="lead">le monde est la matière, l'espace et les phénomènes qui nous sont accessibles par les sens, l'expérience ou la raison.</p>
+        <p class="lead">Le sens le plus courant désigne notre planète, la Terre, avec ses habitants, et son environnement plus ou moins naturel.</p>
+        <hr class="my-4">
+
+        <p>Le sens étendu désigne l'univers dans son ensemble.</p>
+        <a class="btn btn-danger btn-lg" href="#" role="button">Rebooter le Monde</a>
+        <div class="spinner-border text-info" role="status">
+        <span class="sr-only">Loading...</span>
+        </div>
+
+        <!-- Pagination Button -->
+        <ul class="pagination justify-content-end">
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+            </a>
+            </li>
+        </ul>
+        <!-- End Pagination -->
+    </div>
+        <div class="">
+            <ul class="list-group">
+                <li class="list-group-item active">Limbes</li>
+                <li class="list-group-item">Luxure</li>
+                <li class="list-group-item">Gourmandise</li>
+                <li class="list-group-item">Avarice</li>
+                <li class="list-group-item">Colere</li>
+                <li class="list-group-item">Heresie</li>
+                <li class="list-group-item">Violence</li>
+                <li class="list-group-item">Ruse et Tromperie</li>
+                <li class="list-group-item">Trahison</li>
+                <li class="list-group-item">Internet Explorer</li>
+            </ul>
+        </div> 
+</main>
+
+<!-- Progress Bar -->
+<section class="container d-flex flex-column align-items-center">
+    <div class="d-flex justify-content-end w-75">
+        <h5>Installation de AI 9000</h5>
+    </div>
+    <div class="d-flex justify-content-center w-100">
+        <svg class="bi bi-arrow-bar-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M5.854 4.646a.5.5 0 00-.708 0l-3 3a.5.5 0 000 .708l3 3a.5.5 0 00.708-.708L3.207 8l2.647-2.646a.5.5 0 000-.708z" clip-rule="evenodd"/>
+            <path fill-rule="evenodd" d="M10 8a.5.5 0 00-.5-.5H3a.5.5 0 000 1h6.5A.5.5 0 0010 8zm2.5 6a.5.5 0 01-.5-.5v-11a.5.5 0 011 0v11a.5.5 0 01-.5.5z" clip-rule="evenodd"/>
+        </svg>
+
+        <div class="progress w-75">
+            <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%"></div>
+        </div>
+            
+        <svg class="bi bi-arrow-bar-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M10.146 4.646a.5.5 0 01.708 0l3 3a.5.5 0 010 .708l-3 3a.5.5 0 01-.708-.708L12.793 8l-2.647-2.646a.5.5 0 010-.708z" clip-rule="evenodd"/>
+            <path fill-rule="evenodd" d="M6 8a.5.5 0 01.5-.5H13a.5.5 0 010 1H6.5A.5.5 0 016 8zm-2.5 6a.5.5 0 01-.5-.5v-11a.5.5 0 011 0v11a.5.5 0 01-.5.5z" clip-rule="evenodd"/>
+        </svg>
+    </div>
+</section>
+<!-- END Progress Bar -->
+
+<br>
+
+<!-- Formulaire -->
+<form class="mt-4 mb-4">
+  <div class="row d-flex justify-content-around w-100">
+    <div class="d-flex flex-column">
+      <h5 class="ml-3">Recevez votre copie gratuite d'internet 2!</h5>
+      <div class="col-auto mb-2">
+        <div class="input-group mb-2">
+          <div class="input-group-prepend">
+            <div class="input-group-text">@</div>
+          </div>
+          <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Login">
+        </div>
+      </div>
+      <div class="col-auto mb-2">
+        <div class="input-group mb-2">
+          <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Mot de Passe">
+          <div class="input-group-prepend">
+            <div class="input-group-text">@example.com</div>
+          </div>
+          
+        </div>
+      </div>
+      <div class="col-auto mb-2">
+          <label for="exampleInputEmail1">URL des Internets 2 et 2.1 Beta</label>
+          <div class="input-group-prepend mb-2">
+            <div class="input-group-text">DogeCoin</div>
+            <input type="text" class="form-control" id="inlineFormInputGroup">
+            <div class="input-group-text">.00</div>
+          </div>
+      </div>
+      <div class="col-auto">
+          <div class="input-group-prepend">
+            <div class="input-group-text">https://133.lptf/dkwb/berlusconimkt/</div>
+            <input type="text" class="form-control" id="inlineFormInputGroup">
+          </div>  
+      </div>
+    </div>
+    <div class="d-flex flex-column">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <div class="col-auto">
+        <button type="submit" class="btn btn-primary mb-2">Submit</button>
+      </div>
+    </div>
+  </div>
+</form>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
